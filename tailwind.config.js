76a2035012d5dac0338d5f3a46c95e6eacdabd66
Fasakin Henry <fasakinhenry@gmail.com>
2024-07-26 22:52:54 +0100

    styles: add all the required font family in tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,14 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-  ],
+  content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        bricolage: ['Bricolage Grotesque', 'sans-serif'],
+        inter: ['Inter', 'sans-serif'],
+        'ibm-plex': ['IBM Plex Sans', 'sans-serif'],
+      },
+    },
   },
   plugins: [],
-}
+};
