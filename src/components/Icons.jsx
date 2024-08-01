81341dcb4579acd3118aa2cad21724941a565deb
Fasakin Henry <fasakinhenry@gmail.com>
2024-08-01 22:57:08 +0100

    Add Icons components for all login Icons

---
+++ src/components/Icons.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+
+export const GoogleIcon = (props) => (
+  <svg {...props} width="20" height="20" viewBox="0 0 48 48">
+    {/* Google SVG Path */}
+  </svg>
+);
+
+export const MicrosoftIcon = (props) => (
+  <svg {...props} width="20" height="20" viewBox="0 0 48 48">
+    {/* Microsoft SVG Path */}
+  </svg>
+);
+
+export const SecurityIcon = (props) => (
+  <svg {...props} width="20" height="20" viewBox="0 0 48 48">
+    {/* Security SVG Path */}
+  </svg>
+);
