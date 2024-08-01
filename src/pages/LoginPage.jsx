9d05429fa1975f7e7144d04e1cea63feaa97fc0b
Fasakin Henry <fasakinhenry@gmail.com>
2024-08-02 00:37:45 +0100

    chore: add begg wallet screen img

--- src/pages/LoginPage.jsx
+++ src/pages/LoginPage.jsx
@@ -5,12 +5,13 @@ const LoginPage = () => {
     <div className='flex min-h-screen bg-[#000207] text-white'>
       {/* Left side */}
       <div className='flex flex-col w-full md:w-3/5 p-8'>
-        <header className='mb-16'>
-          <img
-            src='/img/fireflies-logo.png'
+        <header className='flex mb-16 items-center'>
+          {/* <img
+            src='/logo.png'
             alt='fireflies.ai'
-            className='h-8'
-          />
+            className='h-10'
+          /> */}
+          <div className='font-bricolage ml-5 text-xl font-bold'>Begg</div>
         </header>
         <main className='flex-grow flex flex-col justify-center max-w-md mx-auto w-full'>
           <h1 className='text-4xl font-bold mb-4 font-bricolage'>
@@ -21,12 +22,12 @@ const LoginPage = () => {
             conversations.
           </p>
           <button className='flex items-center justify-center bg-[#fdfdfd] bg-opacity-10 hover:bg-opacity-20 text-white rounded-md py-3 mb-4 transition-colors font-inter'>
-            <img src='/img/google-icon.png' alt='' className='mr-2 h-5 w-5' />
+            <img src='/img/Google.svg' alt='' className='mr-2 h-5 w-5' />
             Continue with Google
           </button>
           <button className='flex items-center justify-center bg-[#fdfdfd] bg-opacity-10 hover:bg-opacity-20 text-white rounded-md py-3 mb-4 transition-colors font-inter'>
             <img
-              src='/img/microsoft-icon.png'
+              src='/img/microsoft.svg'
               alt=''
               className='mr-2 h-5 w-5'
             />
@@ -67,16 +68,16 @@ const LoginPage = () => {
       {/* Right side */}
       <div className='hidden md:flex md:w-2/5 bg-gray-900 relative flex-col items-center justify-center'>
         <img
-          src='/img/background-image.png'
+          src='/img/login-background.png'
           alt='Background'
           className='absolute inset-0 w-full h-full object-cover'
         />
         <img
-          src='/img/product-team-sync.png'
+          src='/img/begg-wallet.png'
           alt='Product Team Sync'
-          className='relative z-10 max-w-sm w-full mb-8'
+          className='relative z-10 max-w-sm max-h-[80vh]'
         />
-        <div className='relative z-10 bg-gray-800 rounded-lg p-4 max-w-sm text-center'>
+        {/* <div className='relative z-10 bg-gray-800 rounded-lg p-4 max-w-sm text-center'>
           <img
             src='/img/vercel-logo.png'
             alt='Vercel'
@@ -97,7 +98,7 @@ const LoginPage = () => {
               <p className='text-sm text-gray-400 font-inter'>Head of Growth</p>
             </div>
           </div>
-        </div>
+        </div> */}
       </div>
     </div>
   );
