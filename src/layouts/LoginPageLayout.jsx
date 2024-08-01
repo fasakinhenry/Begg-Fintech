b72f3a94a17fd0f65063343083b548b0e8b815d9
Fasakin Henry <fasakinhenry@gmail.com>
2024-08-01 23:14:36 +0100

    update Login page layout component
    This fixs the unequal reponsive issues

--- src/layouts/LoginPageLayout.jsx
+++ src/layouts/LoginPageLayout.jsx
@@ -2,8 +2,8 @@ import React from 'react';
 
 const LoginPageLayout = ({ children }) => {
   return (
-    <div className='flex items-center justify-center min-h-screen bg-black text-white'>
-      <div className='flex flex-col w-full max-w-4xl md:flex-row'>
+    <div className="flex items-center justify-center min-h-screen bg-black text-white">
+      <div className="flex flex-col w-full h-full md:flex-row">
         {children}
       </div>
     </div>
