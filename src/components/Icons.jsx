5c3335cf7275e71b3332f2758ce93c73ab62d3c3
Fasakin Henry <fasakinhenry@gmail.com>
2024-08-01 23:04:12 +0100

    update the Icons component

--- src/components/Icons.jsx
+++ src/components/Icons.jsx
@@ -1,19 +1,38 @@
 import React from 'react';
 
 export const GoogleIcon = (props) => (
-  <svg {...props} width="20" height="20" viewBox="0 0 48 48">
-    {/* Google SVG Path */}
+  <svg {...props} viewBox='0 0 48 48' width='24' height='24'>
+    <path
+      fill='#4285F4'
+      d='M24 9.5c2.06 0 3.95.73 5.44 1.96l4.06-4.06C30.58 5.17 27.5 4 24 4 14.61 4 7 11.61 7 21s7.61 17 17 17c8.77 0 16-7.23 16-16 0-1.39-.19-2.75-.52-4.04H24v7.5h13.6c-1.59 4.94-6.26 8.5-13.6 8.5-7.18 0-13-5.82-13-13s5.82-13 13-13z'
+    />
   </svg>
 );
 
 export const MicrosoftIcon = (props) => (
-  <svg {...props} width="20" height="20" viewBox="0 0 48 48">
-    {/* Microsoft SVG Path */}
+  <svg {...props} viewBox='0 0 48 48' width='24' height='24'>
+    <path fill='#F25022' d='M6 6h18v18H6z' />
+    <path fill='#7FBA00' d='M24 6h18v18H24z' />
+    <path fill='#00A4EF' d='M6 24h18v18H6z' />
+    <path fill='#FFB900' d='M24 24h18v18H24z' />
   </svg>
 );
 
 export const SecurityIcon = (props) => (
-  <svg {...props} width="20" height="20" viewBox="0 0 48 48">
-    {/* Security SVG Path */}
+  <svg {...props} viewBox='0 0 24 24' width='24' height='24'>
+    <path
+      d='M12 1L3 5v6c0 5.7 3.9 10.8 9 12 5.1-1.2 9-6.3 9-12V5l-9-4z'
+      fill='#4CAF50'
+    />
+    <path
+      d='M12 1v16.1c-2.7-1-4.9-4.1-5-7.1V5.3L12 3V1z'
+      fill='#FFF'
+      opacity='.2'
+    />
+    <path
+      d='M12 1v16.1c2.7-1 4.9-4.1 5-7.1V5.3L12 3V1z'
+      fill='#FFF'
+      opacity='.5'
+    />
   </svg>
 );
